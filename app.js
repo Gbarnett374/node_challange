97c9ab5dd868337ae33ac280f795fff18966d592
Gbarnett374 <gbarnett374@gmail.com>
2016-12-16 15:52:45 -0500

    some refactors.

--- app.js
+++ app.js
@@ -6,21 +6,27 @@ const bodyParser = require('body-parser');
 app.set('view engine', 'ejs');
 
 // Need this to parse the incoming post data. 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 server.listen(8888, () => {
-  console.log('I am running.');
+  console.log('We are up and running on port 8888.');
 });
 
 app.get('/', (req, res) => {
-  console.log(req.session);
+  let name;
+  let counter;
+  if (req.query.name) {
+    name = req.query.name;
+  }
   res.render('index', { 
-    name: '',
-    counter: '' 
+    name: name,
+    counter: counter
   });
 });
 
 app.post('/hello', (req, res) => {
+  let name;
+  let counter;
   if (req.body.hidden_name) {
     name = req.body.hidden_name + ' ' + req.body.name;
     counter = parseInt(req.body.counter) + 1;
@@ -28,7 +34,7 @@ app.post('/hello', (req, res) => {
     name = req.body.name
     counter = 1;
   }
-  console.log(res.rawHeaders);
+
   res.render('index', { 
     name: name,
     counter: counter 
